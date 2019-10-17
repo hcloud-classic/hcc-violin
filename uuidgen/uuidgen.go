88406285d379f37e0bac8b1897033b8dbb94ba76
ish <ish@innogrid.com>
2019-10-17 21:06:13 +0900

    uuidgen: Check duplicated uuid

--- uuidgen/uuidgen.go
+++ uuidgen/uuidgen.go
@@ -1,18 +1,66 @@
 package uuidgen
 
 import (
+	"github.com/nu7hatch/gouuid"
 	"hcc/violin/logger"
-
-	uuid "github.com/nu7hatch/gouuid"
+	"hcc/violin/mysql"
 )
 
-// Uuidgen : Generate uuid
-func Uuidgen() (string, error) {
-	out, err := uuid.NewV4()
+func checkDuplicateUUID(uuid string) (bool, error) {
+	sql := "select uuid from server"
+	stmt, err := mysql.Db.Query(sql)
 	if err != nil {
-		logger.Log.Println(err)
-		return "", err
+		logger.Logger.Println(err)
+		return false, err
+	}
+	defer func() {
+		_ = stmt.Close()
+	}()
+
+	var dbUUID string
+	var found = false
+	for stmt.Next() {
+		err := stmt.Scan(&dbUUID)
+		if err != nil {
+			logger.Logger.Println(err)
+			return false, err
+		}
+
+		// logger.Logger.Printf("checkDuplicateUUID(): checking for UUID=%s\n", dbUUID)
+
+		if uuid == dbUUID {
+			logger.Logger.Println("checkDuplicateUUID(): Found already existed UUID")
+			found = true
+			break
+		}
 	}
 
-	return out.String(), nil
+	return found, nil
+}
+
+// UUIDgen : Generate uuid
+func UUIDgen() (string, error) {
+	var UUID string
+	for {
+		out, err := uuid.NewV4()
+		if err != nil {
+			logger.Logger.Println(err)
+			return "", err
+		}
+
+		logger.Logger.Println("UUIDgen(): Checking duplicated UUID")
+
+		found, err := checkDuplicateUUID(out.String())
+		if err != nil {
+			logger.Logger.Println(err)
+			return "", err
+		}
+
+		if !found {
+			UUID = out.String()
+			break
+		}
+	}
+
+	return UUID, nil
 }
--- uuidgen/uuidgen_test.go
+++ uuidgen/uuidgen_test.go
@@ -1,9 +1,36 @@
 package uuidgen
 
-import "testing"
+import (
+	"hcc/violin/checkroot"
+	"hcc/violin/config"
+	"hcc/violin/logger"
+	"hcc/violin/mysql"
+	"testing"
+)
 
-func Test_Uuidgen(t *testing.T) {
-	_, err := Uuidgen()
+func Test_UUIDgen(t *testing.T) {
+	if !checkroot.CheckRoot() {
+		t.Fatal("Failed to get root permission!")
+	}
+
+	if !logger.Prepare() {
+		t.Fatal("Failed to prepare logger!")
+	}
+	defer func() {
+		_ = logger.FpLog.Close()
+	}()
+
+	config.Parser()
+
+	err := mysql.Prepare()
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = mysql.Db.Close()
+	}()
+
+	_, err = UUIDgen()
 	if err != nil {
 		t.Fatal("Failed to generate uuid!")
 	}
