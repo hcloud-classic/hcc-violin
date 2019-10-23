c1fc010c58529c9ac6a303558ae31fa5fe81314b
EdwardSon <ehdwn1991@gmail.com>
2019-10-23 12:13:57 +0900

    [viola rabbitmq publisher] RunHccCLI : Action Send to viola

--- action/rabbitmq/publisher.go
+++ action/rabbitmq/publisher.go
@@ -2,10 +2,44 @@ package rabbitmq
 
 import (
 	"encoding/json"
-	"github.com/streadway/amqp"
 	"hcc/violin/lib/logger"
 	"hcc/violin/model"
+
+	"github.com/streadway/amqp"
 )
+
+// RunHccCLI : Publish 'run_hcc_cli' queues to RabbitMQ channel
+func RunHccCLI(action []model.Control) error {
+	qCreate, err := Channel.QueueDeclare(
+		"run_hcc_cli",
+		false,
+		false,
+		false,
+		false,
+		nil)
+	if err != nil {
+		logger.Logger.Println("run_hcc_cli: Failed to declare a create queue")
+		return err
+	}
+
+	body, _ := json.Marshal(action)
+	err = Channel.Publish(
+		"",
+		qCreate.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:     "text/plain",
+			ContentEncoding: "utf-8",
+			Body:            body,
+		})
+	if err != nil {
+		logger.Logger.Println("get_nodes: Failed to register publisher")
+		return err
+	}
+
+	return nil
+}
 
 // GetNodes : Publish 'get_nodes' queues to RabbitMQ channel
 func GetNodes(nodeNr int, serverUUID string) error {
