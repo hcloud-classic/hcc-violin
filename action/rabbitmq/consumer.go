3841b52b914eb0add7819b4f7feeeac1a37d2340
ish <ish@innogrid.com>
2019-10-29 14:36:44 +0900

    rabbitmq: Remove unused functions

--- action/rabbitmq/consumer.go
+++ action/rabbitmq/consumer.go
@@ -9,46 +9,7 @@ import (
 	"log"
 )
 
-// ReturnNodes : Consume 'return_nodes' queues from RabbitMQ channel
-func ReturnNodes() error {
-	qCreate, err := Channel.QueueDeclare(
-		"return_nodes",
-		false,
-		false,
-		false,
-		false,
-		nil)
-	if err != nil {
-		logger.Logger.Println("return_nodes: Failed to declare a create queue")
-		return err
-	}
-
-	msgsCreate, err := Channel.Consume(
-		qCreate.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		logger.Logger.Println("return_nodes: Failed to register consumer")
-		return err
-	}
-
-	go func() {
-		for d := range msgsCreate {
-			log.Printf("return_nodes: Received a create message: %s", d.Body)
-
-			// CreateVolume()
-		}
-	}()
-
-	return nil
-}
-
-//ConsumeViola : Consume Viola command
+// ConsumeViola : Consume Viola command
 func ConsumeViola() error {
 	qCreate, err := Channel.QueueDeclare(
 		"consume_viola",
