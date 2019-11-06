be548a04de0363ded9a66172ec55832269c2e1e7
ish <ish@innogrid.com>
2019-11-06 18:26:43 +0900

    main: Remove unneeded GOMAXPROCS setting

--- main.go
+++ main.go
@@ -8,14 +8,10 @@ import (
 	"hcc/violin/lib/mysql"
 	"hcc/violin/lib/syscheck"
 	"net/http"
-	"runtime"
 	"strconv"
 )
 
 func main() {
-	// Use max CPUs
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
 	if !syscheck.CheckRoot() {
 		return
 	}
