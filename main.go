eadb98f39947521a73c937d118c16aa3d781d437
ish <ish@innogrid.com>
2019-11-07 15:51:41 +0900

    main: Move server runnung message to correctly position

--- main.go
+++ main.go
@@ -59,9 +59,9 @@ func main() {
 
 	http.Handle("/graphql", graphql.GraphqlHandler)
 
-	logger.Logger.Println("Server is running on port " + strconv.Itoa(int(config.HTTP.Port)))
 	err = http.ListenAndServe(":"+strconv.Itoa(int(config.HTTP.Port)), nil)
 	if err != nil {
 		logger.Logger.Println("Failed to prepare http server!")
 	}
+	logger.Logger.Println("Server is running on port " + strconv.Itoa(int(config.HTTP.Port)))
 }
