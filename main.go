d1cb4bc85f29d8da291a89d26a75d694c7a6ff8d
ish <ish@innogrid.com>
2019-11-09 17:13:42 +0900

    main: Separate init work

--- main.go
+++ main.go
@@ -10,18 +10,20 @@ import (
 	"strconv"
 )
 
-func main() {
+func init() {
 	err := violinInit.MainInit()
-	defer func() {
-		violinEnd.MainEnd()
-	}()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	defer func() {
+		violinEnd.MainEnd()
+	}()
 
 	http.Handle("/graphql", graphql.GraphqlHandler)
-
-	err = http.ListenAndServe(":"+strconv.Itoa(int(config.HTTP.Port)), nil)
+	err := http.ListenAndServe(":"+strconv.Itoa(int(config.HTTP.Port)), nil)
 	if err != nil {
 		logger.Logger.Println(err)
 		logger.Logger.Println("Failed to prepare http server!")
