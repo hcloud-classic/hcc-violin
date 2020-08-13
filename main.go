8792bccdf76524eeaedbecc08e019dd379d916c4
ish <ish@innogrid.com>
2020-08-13 17:14:16 +0900

    Revert "main: Run the gRPC server in go routine and catch the exit signal"
    This reverts commit 39bc21b96c1311f3434523548be562ced62afa5d.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"hcc/violin/action/rabbitmq"
 	"hcc/violin/driver/grpccli"
 	"hcc/violin/driver/grpcsrv"
@@ -9,9 +8,6 @@ import (
 	"hcc/violin/lib/logger"
 	"hcc/violin/lib/mysql"
 	"hcc/violin/lib/syscheck"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func init() {
@@ -37,32 +33,19 @@ func init() {
 		panic(err)
 	}
 
-	go grpcsrv.Init()
 	err = grpccli.InitGRPCClient()
 	if err != nil {
 		panic(err)
 	}
 }
 
-func end() {
-	grpccli.CleanGRPCClient()
-	rabbitmq.End()
-	mysql.End()
-	logger.End()
-}
-
 func main() {
-	// Catch the exit signal
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-	go func(){
-		<- sigChan
-		end()
-		fmt.Println("Exiting violin module...")
-		os.Exit(0)
+	defer func() {
+		grpccli.CleanGRPCClient()
+		rabbitmq.End()
+		mysql.End()
+		logger.End()
 	}()
 
-	// Prevent to exit main thread
-	mainChan := make(chan bool)
-	<-mainChan
+	grpcsrv.Init()
 }
