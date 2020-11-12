907ef11799dd168fd448514327e6d041873f4808
ish <ish@innogrid.com>
2020-11-12 15:07:12 +0900

    [Hcloud MiddleWare] Violin Init Handler
     - Add new init handler

---
+++ init/mainInit.go
@@ -0,0 +1,30 @@
+package init
+
+import "hcc/violin/lib/config"
+
+// MainInit : Main initialization function
+func MainInit() error {
+	err := syscheckInit()
+	if err != nil {
+		return err
+	}
+
+	err = loggerInit()
+	if err != nil {
+		return err
+	}
+
+	config.Parser()
+
+	err = mysqlInit()
+	if err != nil {
+		return err
+	}
+
+	err = rabbitmqInit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
