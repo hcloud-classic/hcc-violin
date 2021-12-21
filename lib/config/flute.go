28328dc8338f01c8ad0f8b567c4246c91a18e420
jollaman999 <admin@jollaman999.com>
2021-12-21 14:04:25 +0900

    config: Fix a typo in the comment

--- lib/config/flute.go
+++ lib/config/flute.go
@@ -4,7 +4,7 @@ type flute struct {
 	ServerAddress               string `goconf:"flute:flute_server_address"`                   // ServerAddress : IP address of server which installed flute module
 	ServerPort                  int64  `goconf:"flute:flute_server_port"`                      // ServerPort : Listening port number of flute module
 	RequestTimeoutMs            int64  `goconf:"flute:flute_request_timeout_ms"`               // RequestTimeoutMs : Timeout for gRPC request to flute module
-	TurnOffNodesWaitTimeSec     int64  `goconf:"flute:flute_turn_off_nodes_wait_time_sec"`     // TurnOffNodesWaitTimeSec : Wait time of turning of nodes
+	TurnOffNodesWaitTimeSec     int64  `goconf:"flute:flute_turn_off_nodes_wait_time_sec"`     // TurnOffNodesWaitTimeSec : Wait time of turning off nodes
 	TurnOffNodesRetryCounts     int64  `goconf:"flute:flute_turn_off_nodes_retry_counts"`      // TurnOffNodesRetryCounts : Retry counts of turning off nodes
 	TurnOnNodesRetryCounts      int64  `goconf:"flute:flute_turn_on_nodes_retry_counts"`       // TurnOnNodesRetryCounts : Retry counts of turning on nodes
 	WaitForLeaderNodeTimeoutSec int64  `goconf:"flute:flute_wait_for_leader_node_timeout_sec"` // WaitForLeaderNodeTimeoutSec : Waiting timeout for turn on compute nodes after leader node turned on
